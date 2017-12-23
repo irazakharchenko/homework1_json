746d8c274bd5dcc0b26a25fa5071285c23fe9661
irazakharchenko <zakharchenko@ucu.edu.ua>
2017-12-23 08:44:50 +0200

    part 1 1,2

--- src/main/java/json/JsonObject.java
+++ src/main/java/json/JsonObject.java
@@ -1,22 +1,38 @@
 package json;
 
+import java.util.ArrayList;
+
 /**
  * Created by Andrii_Rodionov on 1/3/2017.
  */
 public class JsonObject extends Json {
+    private ArrayList<JsonPair> ajpairs = new ArrayList<JsonPair>();
 
     public JsonObject(JsonPair... jsonPairs) {
-        // ToDo
+
+        for (JsonPair jp:
+             jsonPairs) {
+            this.add(jp);
+        }
     }
 
     @Override
     public String toJson() {
-        // ToDo
-        return null;
+        String to_return = new String();
+        to_return += '{';
+        for (JsonPair jp:
+             ajpairs) {
+            to_return += jp.key + ": " + jp.value.toJson().toString() + ", ";
+
+        }
+        if(to_return.length() > 1)
+            to_return = to_return.substring(0, to_return.length()-2);
+        to_return += '}';
+        return to_return;
     }
 
     public void add(JsonPair jsonPair) {
-        // ToDo
+        ajpairs.add(jsonPair);
     }
 
     public Json find(String name) {
